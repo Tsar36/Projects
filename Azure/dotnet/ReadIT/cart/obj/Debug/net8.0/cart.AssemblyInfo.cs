29a0bcc78d32ebcb2df3a66415093df0a4fc0c83
TsaR <seriy.tsar@gmail.com>
2024-12-10 16:32:41 +0200

    added gitignore

--- Azure/dotnet/ReadIT/cart/obj/Debug/net8.0/cart.AssemblyInfo.cs
+++ Azure/dotnet/ReadIT/cart/obj/Debug/net8.0/cart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fc484aa3f9b5e4feef236d06f9dee7f0767644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18933efc38b1dc5f64a4a5520252a24bbb072f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("cart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
